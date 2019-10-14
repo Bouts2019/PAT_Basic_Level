893195a0ba6a34537e241de97f03b8533bfeef16
Bouts2019 <48877891+Bouts2019@users.noreply.github.com>
2019-10-14 12:35:21 +0800

    Update Main.java

--- src/com/pat/task14/Main.java
+++ src/com/pat/task14/Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // 在一行中输出约会的时间，格式为 DAY HH:MM，其中 DAY 是某星期的 3 字符缩写，即 MON 表示星期一，TUE 表示星期二，WED 表示星期三，
-        // THU 表示星期四，FRI 表示星期五，SAT 表示星期六，SUN 表示星期日。题目输入保证每个测试存在唯一解。
         try {
             while (in.hasNext()) {
                 String dateAndHourStr0 = in.next();
@@ -18,20 +16,37 @@ public class Main {
                 int index = 0;
                 int minLen = Math.min(dateAndHourStr0.length(), dateAndHourStr1.length());
 
-                for (int i = 0; i < minLen; i++)
-                    if (dateAndHourStr0.charAt(i) == dateAndHourStr1.charAt(i) && dateAndHourStr0.charAt(i) >= 65 && dateAndHourStr0.charAt(i) <= 90)
-                        dateAndHourChars[index++] = dateAndHourStr0.charAt(i);
+                for (int i = 0; i < minLen; i++) {
+                    if (index == 0) {
+                        if (dateAndHourStr0.charAt(i) == dateAndHourStr1.charAt(i) && (dateAndHourStr0.charAt(i) >= 65 && dateAndHourStr0.charAt(i) <= 71)) {
+                            dateAndHourChars[index++] = dateAndHourStr0.charAt(i);
+                        }
+                    }
+                    else if (index == 1) {
+                        if (dateAndHourStr0.charAt(i) == dateAndHourStr1.charAt(i) && ((dateAndHourStr0.charAt(i) >= 65 && dateAndHourStr0.charAt(i) <= 78) || (dateAndHourStr0.charAt(i) >= '0' && dateAndHourStr0.charAt(i) <= '9'))) {
+                            dateAndHourChars[index++] = dateAndHourStr0.charAt(i);
+                            break;
+                        }
+                    }
+
+                }
+
 
                 minLen = Math.min(minStr0.length(), minStr1.length());
 
-                char min = 0;
+                int minIndex = 0;
 
                 for (int i = 0; i < minLen; i++)
-                    if (minStr0.charAt(i) == minStr1.charAt(i) && ((minStr0.charAt(i) >= 65 && minStr1.charAt(i) <= 90) || (minStr0.charAt(i) >= 97 && minStr1.charAt(i) <= 122)))
-                        min = minStr0.charAt(i);
+                    if (minStr0.charAt(i) == minStr1.charAt(i) && ((minStr0.charAt(i) >= 65 && minStr1.charAt(i) <= 90) || (minStr0.charAt(i) >= 97 && minStr1.charAt(i) <= 122))) {
+                        minIndex = i;
+                    }
 
-                System.out.println(dateAndHourChars[0] + "," + dateAndHourChars[1] + "," + min);
+                String[] weekStrs= { "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN" };
+                int hour = (dateAndHourChars[1] >= 65 &&  dateAndHourChars[1] <= 90) ? 9 + dateAndHourChars[1] - 'A' + 1: dateAndHourChars[1] - '0';
 
+                System.out.print(weekStrs[dateAndHourChars[0] - 'A'] + " " + String.format("%02d", hour) + ":" + String.format("%02d", minIndex));
+
+                return;
             }
         } catch (NullPointerException e) {
             e.printStackTrace();
