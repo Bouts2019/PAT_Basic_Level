3806094ef9b3c9d90e269174b945f164d8b966c0
Bouts2019 <48877891+Bouts2019@users.noreply.github.com>
2019-10-15 23:37:20 +0800

    Create main.cpp

---
+++ src/com/pat/task33/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int asciiTable[128] = { 0 };
+    int len = 0;
+    string brokenKey;
+    string input;
+
+    getline(cin, brokenKey);
+    len = brokenKey.length();
+    for (int i = 0; i < len; i++)
+        if (brokenKey[i] != '+')
+            if (brokenKey[i] >= 'A' && brokenKey[i] <= 'Z')
+            {
+                asciiTable[brokenKey[i]] = 1;
+                asciiTable[brokenKey[i] + 'a' - 'A'] = 1;
+            }
+            else asciiTable[brokenKey[i]] = 1;
+        else for (int j = 0; j < 26; j++) asciiTable['A' + j] = 1;
+    getline(cin, input);
+    len = input.length();
+    for (int i = 0; i < len; i++)
+        if (!asciiTable[input[i]])
+            cout << input[i];
+
+    return 0;
+}
